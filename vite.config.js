0d6d8fda0103cecb535923a9c1241e0abf99789d
Kaleo Kuka Romero <kaleo.romero@gmail.com>
2024-06-28 19:22:37 -0300

    vsharp

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import './src/assets/banner.png'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import viteSharp from 'vite-plugin-sharp'
+import vsharp from 'vite-plugin-sharp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), viteSharp()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vsharp({
+      webp: {
+        quality: 80,
+        size: 1024
+      }
+    })
+  ],
   build: {
     rolloupOptions: {
       external: ['/src/assets/banner.png']
